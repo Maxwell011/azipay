afa8667727f3b30bfe7f1684eed9840f0e57a74c
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-08-16 00:09:59 +0100

    created navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href='#home'>Azipay logo</a>
+        </li>
+        <li>
+          <a href='#home'>Individual</a>
+        </li>
+        <li>
+          <a href='#about'>Business</a>
+        </li>
+        <li>
+          <a href='#services'>Pricing</a>
+        </li>
+        <li>
+          <a href='#contact'>Set your payroll</a>
+        </li>
+        <li>
+          <a href='#contact'>Login</a>
+        </li>
+        <li>
+          <a href='#contact'>Register</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
