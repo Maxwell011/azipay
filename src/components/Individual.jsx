7d4e013f16c39035b47c75e2b2a17aa83dee87dd
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-08-21 23:46:29 +0100

    Updated individual page

--- src/components/Individual.jsx
+++ src/components/Individual.jsx
@@ -1,6 +1,50 @@
+import Footer from "./Footer";
+
 const Individual = () => {
   return (
-    <div>Individual</div>
-  )
-}
-export default Individual
+    <div>
+      {/* Background landing image */}
+      <img src='' alt='' />
+      <h2>EAZIPAY FOR INDIVIDUAL</h2>
+      <h1>
+        Pay all in <span>one click!</span>
+      </h1>
+      <p>
+        Add your service providers, personal assistants, chauffeurs, chefs, and
+        more to your Eazipay. Pay all in one click
+      </p>
+      <button>Start using Free, Forever</button>
+
+      {/* Section 2 */}
+      <div>
+        <h2>LOANS</h2>
+        <h1>Loans, wherever you need them</h1>
+        <p>
+          Whenever you fall short of your monthly payroll budget you may easily
+          request a loan to cover for it.
+        </p>
+      </div>
+      <img src='' alt='' />
+      {/* Section 3 */}
+      <div>
+        <h2>BILLS AND PAYMENTS</h2>
+        <h1>Fast Transfer, Bills and Payments</h1>
+        <p>
+          Pay all relevant bills, send to anyone and save your loved ones as
+          beneficiaries
+        </p>
+        <div className='boxes'>
+          <div className='box'>Airtime</div>
+          <div className='box'>Data</div>
+          <div className='box'>Electricity</div>
+          <div className='box'>Gotv/Dstv</div>
+          <div className='box'>Make a Transfer</div>
+          <div className='box'>Pay Salaries</div>
+          <div className='box'>Get Loan</div>
+        </div>
+      </div>
+			<Footer />
+    </div>
+  );
+};
+export default Individual;
