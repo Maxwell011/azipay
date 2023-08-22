d90604fbf560b4b3a78f3f6c0a78ccfe5635737d
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-08-22 21:30:53 +0100

    updated business page

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,6 +1,42 @@
+import Nav from "./Nav"
+
 const Business = () => {
   return (
-    <div>Business</div>
-  )
+    <div>
+      <Nav />
+      {/* section 1 */}
+      {/* TODO: */}
+      <img src='' alt='' />
+      <h2>EAZIPAY FOR BUSINESS</h2>
+      <h1>Simple Payroll</h1>
+      <p>
+        Add your employee the simple way you know and run their payroll. All in
+        seconds!
+      </p>
+      <button>Start Using Free, Forever</button>
+      {/* section 2 */}
+      <img src='' alt='' />
+      <h2>ENTERPRISE LOANS</h2>
+      <h1>Payroll & Enterprise loans</h1>
+      <p>
+        Easily access loans to cover your operations or payroll, right on your
+        app!
+      </p>
+      {/* section 3*/}
+      <img src='' alt='' />
+      <h2>REPORT</h2>
+      <h1>Accurate Payroll reports & Payslips</h1>
+      <p>
+        For making inteligent business decisons, Get your payroll report, employee payslips, tax recipts, payroll analysis, compliance history - all in one place!
+      </p>
+      {/* section 4*/}
+      <img src='' alt='' />
+      <h2>COMPLIANCE</h2>
+      <h1>Register an Remit compliances easily</h1>
+      <p>
+        Register your 
+      </p>
+    </div>
+  );
 }
 export default Business
