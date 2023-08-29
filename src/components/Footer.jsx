5819869bfccd7882ffd681a305e5dc9cdb285983
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-08-29 23:33:17 +0100

    Added styles to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,32 +5,34 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div>
+        <div className="contact-us">
           <p className="contact">Contact Us</p>
           <ul>
             <li className="gmail">ezipay@gmail.com</li>
-            <li>+2348168768762</li>
-            <input type='text' />
+            <li className="number">+2348168768762</li>
+            <input type='text' placeholder="Your Email Address" />
           </ul>
         </div>
 
+        <div className="services">
         <div>
-          <p>Product</p>
+          <p className="contact">Product</p>
           <ul>
             <li>Individual</li>
             <li>Business</li>
-            <li>Request Demo</li>
-            <li>Pricing</li>
+            <l className="desktop">Request Demo</l>
+            <li className="desktop">Pricing</li>
           </ul>
         </div>
         <div>
-          <p>Legal</p>
+          <p className="contact">Legal</p>
           <ul>
             <li>Privacy Policy</li>
             <li>Terms Of Services</li>
           </ul>
         </div>
-        <div>
+        </div>       
+        <div className="desktop">
           <p>Resources</p>
           <ul>
             <li>FAQs</li>
@@ -42,10 +44,10 @@ const Footer = () => {
         <div className=''>
           <p>Azipay logo</p>
           <p>&copy; 2023 Your Website. All rights reserved.</p>
-          <FaInstagram />
-          <FaTwitter />
-          <FaLinkedinIn />
-          <FaFacebookF />
+          <FaInstagram className="social-icons"/>
+          <FaTwitter className="social-icons"/>
+          <FaLinkedinIn className="social-icons"/>
+          <FaFacebookF className="social-icons"/>
         </div>
       </footer>
     </>
