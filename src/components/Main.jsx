b20a34a031c77ef137c5803a7703b9864c080c7f
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-09-06 22:32:39 +0100

    Changed form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -113,37 +113,66 @@ const Main = () => {
               We want to serve you better. Tell us a bit about yourself or
               company
             </p>
-            <form className="form">
-    <p className="title">Register </p>
-    <p className="message">Signup now and get full access to our app. </p>
-        <div className="flex">
-        <label>
-            <input required="" placeholder="" type="text" className="input" />
-            <span>Firstname</span>
-        </label>
+            <form className='form'>
+              <p className='title'>Register </p>
+              <p className='message'>
+                Signup now and get full access to our app.{" "}
+              </p>
+              <div className='flex'>
+                <label>
+                  <input
+                    required=''
+                    placeholder=''
+                    type='text'
+                    className='input'
+                  />
+                  <span>Firstname</span>
+                </label>
 
-        <label>
-            <input required="" placeholder="" type="text" className="input"/>
-            <span>Lastname</span>
-        </label>
-    </div>  
-            
-    <label>
-        <input required="" placeholder="" type="email" className="input" />
-        <span>Email</span>
-    </label> 
-        
-    <label>
-        <input required="" placeholder="" type="password" className="input" />
-        <span>Password</span>
-    </label>
-    <label>
-        <input required="" placeholder="" type="password" className="input" />
-        <span>Confirm password</span>
-    </label>
-    <button className="submit">Submit</button>
-    <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
-</form>
+                <label>
+                  <input
+                    required=''
+                    placeholder=''
+                    type='text'
+                    className='input'
+                  />
+                  <span>Lastname</span>
+                </label>
+              </div>
+
+              <label>
+                <input
+                  required=''
+                  placeholder=''
+                  type='email'
+                  className='input'
+                />
+                <span>Email</span>
+              </label>
+
+              <label>
+                <input
+                  required=''
+                  placeholder=''
+                  type='password'
+                  className='input'
+                />
+                <span>Password</span>
+              </label>
+              <label>
+                <input
+                  required=''
+                  placeholder=''
+                  type='password'
+                  className='input'
+                />
+                <span>Confirm password</span>
+              </label>
+              <button className='submit'>Submit</button>
+              <p className='signin'>
+                Already have an acount ? <a href='#'>Signin</a>{" "}
+              </p>
+            </form>
           </div>
         </div>
       </main>
