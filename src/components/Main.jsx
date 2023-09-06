a7876c2c336f2137b6019341a41a45ed40b36731
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-09-06 22:29:57 +0100

    Changed form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -113,13 +113,37 @@ const Main = () => {
               We want to serve you better. Tell us a bit about yourself or
               company
             </p>
-            <form action=''>
-              {/* individual and companies */}
-              <input type='text' placeholder='First Name' />
-              <input type='text' placeholder='Last Name' />
-              <input type='email' placeholder='Email' />
-              <button>Request Demo</button>
-            </form>
+            <form className="form">
+    <p className="title">Register </p>
+    <p className="message">Signup now and get full access to our app. </p>
+        <div className="flex">
+        <label>
+            <input required="" placeholder="" type="text" className="input" />
+            <span>Firstname</span>
+        </label>
+
+        <label>
+            <input required="" placeholder="" type="text" className="input"/>
+            <span>Lastname</span>
+        </label>
+    </div>  
+            
+    <label>
+        <input required="" placeholder="" type="email" className="input" />
+        <span>Email</span>
+    </label> 
+        
+    <label>
+        <input required="" placeholder="" type="password" className="input" />
+        <span>Password</span>
+    </label>
+    <label>
+        <input required="" placeholder="" type="password" className="input" />
+        <span>Confirm password</span>
+    </label>
+    <button className="submit">Submit</button>
+    <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
+</form>
           </div>
         </div>
       </main>
