903da0d311f35dbdf5cde023be668488fd8769ca
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-09-19 23:57:07 +0100

    Updated styles

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -43,7 +43,7 @@ const Business = () => {
       <img src={Report} alt='Payroll Report' className='max-w-full h-auto' />
       <div className='flex flex-col items-center justify-center'>
         <h2 className='text-red-500 text-2xl'>REPORT</h2>
-        <h1 className='text-4xl'>Accurate Payroll reports & Payslips</h1>
+        <h1 className='text-3xl'>Accurate Payroll reports & Payslips</h1>
         <p className='text-xl'>
           For making intelligent business decisions, Get your payroll report,
           employee payslips, tax receipts, payroll analysis, compliance history
@@ -52,43 +52,47 @@ const Business = () => {
       </div>
       {/* section 4*/}
       <img src={Compliance} alt='Compliance' className='max-w-full h-auto' />
-      <div className='flex flex-col items-center justify-center'>
+      <div className='flex flex-col items-center justify-center my-10 mx-2'>
         <h2 className='text-red-500 text-2xl'>COMPLIANCE</h2>
-        <h1 className='text-4xl'>Register an Remit compliances easily</h1>
-        <p className='text-xl'>
+        <h1 className='text-2xl'>Register an Remit compliances easily</h1>
+        <p className='text-sm'>
           Register your business and employees for All your statutory deductions
           - PAYE, Pension, HMO, Insurances, NSITF, NHF.
         </p>
       </div>
       {/* section 5*/}
-      <h2>EAZIPAY PAYROLL CALCULATOR</h2>
-      <h1>Run payroll in Five minutes or less</h1>
-      <p>
-        For making intelligent business decisions, Get your payroll report,
-        employee payslips, tax receipts, payroll analysis, compliance history -
-        all in one place!{" "}
-      </p>
-      <form action=''>
-        <h2>Payroll Calculator</h2>
-        <p>Calculate your payroll either by amount or percentage</p>
-        {/* Double button to switch between amount and percentage */}
-        <input type='text' name='' id='' placeholder='Total Taxable Salary' />
-        <label htmlFor=''>Allowances</label>
-        <input type='text' placeholder='Basic' />
-        <input type='text' placeholder='Housing' />
-        <input type='text' placeholder='Transport' />
-        <input type='text' placeholder='Others' />
-        <label htmlFor=''>Deductions</label>
-        <input type='text' placeholder='HMO ' />
-        <div className='cl-toggle-switch'>
-          <label className='cl-switch'>
-            PAYE
-            <input type='checkbox' />
-            <span></span>
-          </label>
-        </div>
-        <button>Run Payroll</button>
-      </form>
+      <div className='flex flex-col items-center justify-center'>
+        <h2>EAZIPAY PAYROLL CALCULATOR</h2>
+        <h1>Run payroll in Five minutes or less</h1>
+        <p>
+          For making intelligent business decisions, Get your payroll report,
+          employee payslips, tax receipts, payroll analysis, compliance history
+          - all in one place!{" "}
+        </p>
+      </div>
+      <div className='bg-cadetblue p-10 text-center rounded-2xl m-20 h-400'>
+        <form action='' className=''>
+          <h2>Payroll Calculator</h2>
+          <p>Calculate your payroll either by amount or percentage</p>
+          {/* Double button to switch between amount and percentage */}
+          <input type='text' name='' id='' placeholder='Total Taxable Salary' />
+          <label htmlFor=''>Allowances</label>
+          <input type='text' placeholder='Basic' />
+          <input type='text' placeholder='Housing' />
+          <input type='text' placeholder='Transport' />
+          <input type='text' placeholder='Others' />
+          <label htmlFor=''>Deductions</label>
+          <input type='text' placeholder='HMO ' />
+          <div className='cl-toggle-switch'>
+            <label className='cl-switch'>
+              PAYE
+              <input type='checkbox' />
+              <span></span>
+            </label>
+          </div>
+          <button className=''>Run Payroll</button>
+        </form>
+      </div>
     </div>
   );
 };
