ebc3f86a09a03b89a6b5fe95135113f7e2aca724
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-09-19 23:40:20 +0100

    Updated styles

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -9,7 +9,7 @@ const Business = () => {
       {/* section 1 */}
       <section>
         <img src={Heroimage} alt='Hero Image' className='max-w-full h-auto' />
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center flex-col justify-center'>
           <h2 className='text-red-500 text-lg flex items-center justify-center mt-6'>
             EAZIPAY FOR BUSINESS
           </h2>
@@ -20,7 +20,7 @@ const Business = () => {
             Add your employee the simple way you know and run their payroll. All
             in seconds!
           </p>
-          <button className='bg-slate-950 text-white p-3 h-10 text-center rounded-3xl m-5'>
+          <button className='bg-[#11453b] text-white text-base w-135 rounded-full font-medium outline-none h-9 pl-8 pr-8 transition-transform duration-250 ease-in'>
             Start Using Free, Forever
           </button>
         </div>
@@ -40,30 +40,26 @@ const Business = () => {
         </p>
       </div>
       {/* section 3*/}
-      <img
-        src={Report}
-        alt='Payroll Report'
-        className='max-w-full h-auto'
-      />
-      <h2 className='text-red-500 text-2xl'>REPORT</h2>
-      <h1 className='text-4xl'>Accurate Payroll reports & Payslips</h1>
-      <p className='text-xl'>
-        For making intelligent business decisions, Get your payroll report,
-        employee payslips, tax receipts, payroll analysis, compliance history -
-        all in one place!{" "}
-      </p>
+      <img src={Report} alt='Payroll Report' className='max-w-full h-auto' />
+      <div className='flex flex-col items-center justify-center'>
+        <h2 className='text-red-500 text-2xl'>REPORT</h2>
+        <h1 className='text-4xl'>Accurate Payroll reports & Payslips</h1>
+        <p className='text-xl'>
+          For making intelligent business decisions, Get your payroll report,
+          employee payslips, tax receipts, payroll analysis, compliance history
+          - all in one place!{" "}
+        </p>
+      </div>
       {/* section 4*/}
-      <img
-        src={Compliance}
-        alt='Compliance'
-        className='max-w-full h-auto'
-      />
-      <h2 className='text-red-500 text-2xl'>COMPLIANCE</h2>
-      <h1 className='text-4xl'>Register an Remit compliances easily</h1>
-      <p className='text-xl'>
-        Register your business and employees for All your statutory deductions -
-        PAYE, Pension, HMO, Insurances, NSITF, NHF.
-      </p>
+      <img src={Compliance} alt='Compliance' className='max-w-full h-auto' />
+      <div className='flex flex-col items-center justify-center'>
+        <h2 className='text-red-500 text-2xl'>COMPLIANCE</h2>
+        <h1 className='text-4xl'>Register an Remit compliances easily</h1>
+        <p className='text-xl'>
+          Register your business and employees for All your statutory deductions
+          - PAYE, Pension, HMO, Insurances, NSITF, NHF.
+        </p>
+      </div>
       {/* section 5*/}
       <h2>EAZIPAY PAYROLL CALCULATOR</h2>
       <h1>Run payroll in Five minutes or less</h1>
