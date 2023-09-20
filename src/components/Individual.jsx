743ba618c9f7c6e86ed199b9e6cedf1193113267
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-09-20 23:21:05 +0100

    Updated individual page styles

--- src/components/Individual.jsx
+++ src/components/Individual.jsx
@@ -7,24 +7,24 @@ const Individual = () => {
   return (
     <div>
       <img src={IndividualHero} alt='Individual Hero' />
-      <div className='flex flex-col justify-center'>
-        <h2 className='text-red-500 text-2xl'>EAZIPAY FOR INDIVIDUAL</h2>
-        <h1 className='max-w-full h-auto'>
+      <div className='flex items-center flex-col justify-center'>
+        <h2 className='text-red-500 text-lg'>EAZIPAY FOR INDIVIDUAL</h2>
+        <h1 className='max-w-full h-auto text-4xl'>
           Pay all in <span className='text-red-500'>one click!</span>
         </h1>
-        <p className='text-2xl'>
+        <p className='text-base text-center w-96'>
           Add your service providers, personal assistants, chauffeurs, chefs,
           and more to your Eazipay. Pay all in one click
         </p>
         <button className='bg-slate-950 text-white p-3 h-10 text-center rounded-3xl m-5'>
           Start using Free, Forever
         </button>
-      </div>
-      {/* Section 2 */}
-      <div>
-        <h2>LOANS</h2>
-        <h1>Loans, wherever you need them</h1>
-        <p>
+        {/* Section 2 */}
+        <h2 className='text-lg text-red-500'>LOANS</h2>
+        <h1 className='text-4xl text-center'>
+          Loans,<span className='text-red-500'>wherever you need them</span>{" "}
+        </h1>
+        <p className='text-center'>
           Whenever you fall short of your monthly payroll budget you may easily
           request a loan to cover for it.
         </p>
@@ -35,10 +35,10 @@ const Individual = () => {
         className='max-w-full h-auto'
       />
       {/* Section 3 */}
-      <div>
-        <h2>BILLS AND PAYMENTS</h2>
-        <h1>Fast Transfer, Bills and Payments</h1>
-        <p>
+      <div className='flex items-center flex-col justify-center text-center'>
+        <h2 className=''>BILLS AND PAYMENTS</h2>
+        <h1 className=''>Fast Transfer, Bills and Payments</h1>
+        <p className=''>
           Pay all relevant bills, send to anyone and save your loved ones as
           beneficiaries
         </p>
