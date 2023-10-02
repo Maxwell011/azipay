2a4342004f4c03711ac37fc711cdab4383f18b2e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-10-02 20:27:07 +0100

    Added pattern for background

--- src/components/Individual.jsx
+++ src/components/Individual.jsx
@@ -41,65 +41,67 @@ const Individual = () => {
         className='max-w-full h-auto'
       />
       {/* Section 3 */}
-      <div className='flex items-center flex-col justify-center text-center'>
-        <h2 className='text-red-500'>BILLS AND PAYMENTS</h2>
-        <h1 className='text-4xl m-10'>Fast Transfer, Bills and Payments</h1>
-        <p className='w-96 text-center'>
-          Pay all relevant bills, send to anyone and save your loved ones as
-          beneficiaries
-        </p>
-        <div className=''>
-          <div className='bg-red-800 p-10 m-10 h-36 w-48 rounded-lg'>
-            <div className='flex flex-col items-center'>
-              <BiPhoneCall className='bg-[#f2f1f1] h-14 w-14 text-teal-400 text-base rounded-lg p-3' />
-              Airtime
+      <section className="bg-[url('/images/patterns.icons.png')] bg-center bg-cover bg-[#ffeeec]">
+        <div className='flex items-center flex-col justify-center text-center pt-20 pb-20'>
+          <h2 className='text-red-500'>BILLS AND PAYMENTS</h2>
+          <h1 className='text-4xl m-10'>Fast Transfer, Bills and Payments</h1>
+          <p className='w-96 text-center'>
+            Pay all relevant bills, send to anyone and save your loved ones as
+            beneficiaries
+          </p>
+          <div>
+            <div className='bg-red-800 p-10 m-10 h-36 w-48 rounded-lg'>
+              <div className='flex flex-col items-center'>
+                <BiPhoneCall className='bg-[#f2f1f1] h-14 w-14 text-teal-400 text-base rounded-lg p-3' />
+                Airtime
+              </div>
             </div>
-          </div>
-          <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
-            <div className='flex flex-col items-center'>
-              <BiWifi className='text-purple-900  h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3' />
-              Data
+            <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
+              <div className='flex flex-col items-center'>
+                <BiWifi className='text-purple-900  h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3' />
+                Data
+              </div>
             </div>
-          </div>
-          <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
-            <div className='flex flex-col items-center'>
-              <BiBoltCircle className='h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3 text-yellow-500' />
-              Electricity
+            <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
+              <div className='flex flex-col items-center'>
+                <BiBoltCircle className='h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3 text-yellow-500' />
+                Electricity
+              </div>
             </div>
-          </div>
-          <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
-            <div className='flex flex-col items-center'>
-              <FiMonitor className='h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3 text-slate-400' />
-              Gotv/Dstv
+            <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
+              <div className='flex flex-col items-center'>
+                <FiMonitor className='h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3 text-slate-400' />
+                Gotv/Dstv
+              </div>
             </div>
-          </div>
-          <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg '>
-            <div className='flex flex-col items-center'>
-              <FaTelegram className='text-green-700 h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3 ' />
-              Make a Transfer
+            <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg '>
+              <div className='flex flex-col items-center'>
+                <FaTelegram className='text-green-700 h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3 ' />
+                Make a Transfer
+              </div>
             </div>
-          </div>
-          <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
-            <div className='flex flex-col items-center'>
-              <FaMoneyBillWave className='text-slate-600 h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3' />
-              Pay Salaries
+            <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
+              <div className='flex flex-col items-center'>
+                <FaMoneyBillWave className='text-slate-600 h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3' />
+                Pay Salaries
+              </div>
             </div>
-          </div>
-          <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
-            <div className='flex flex-col items-center'>
-              <img
-                src={loanImage}
-                alt='loan image'
-                className='text-slate-600 h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3'
-              />
-              Get Loan
+            <div className='bg-red-800 p-10 m-10 h-40 w-48 rounded-lg'>
+              <div className='flex flex-col items-center'>
+                <img
+                  src={loanImage}
+                  alt='loan image'
+                  className='text-slate-600 h-14 w-14 bg-[#f2f1f1]  text-base rounded-lg p-3'
+                />
+                Get Loan
+              </div>
             </div>
+            <button className='w-48 bg-black text-white rounded-3xl p-2'>
+              Get started now
+            </button>
           </div>
-          <button className='w-48 bg-black text-white rounded-3xl p-2'>
-            Get started now
-          </button>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
