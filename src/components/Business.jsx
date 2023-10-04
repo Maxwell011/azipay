0f8cc9712f5cc391ca8b08ce6cafa14fba5dc674
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-10-04 23:01:37 +0100

    Added height

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,13 +2,13 @@ import HeroImage from "/images/hero-img.d91af7d7.png";
 import Enterprise from "/images/enterprise-loans.44b241cf.png";
 import Report from "/images/report.e1809efd.png";
 import Compliance from "/images/compliance.0c890296.png";
-import "../css/Business.css"
+import "../css/Business.css";
 
 const Business = () => {
   return (
     <div>
       {/* section 1 */}
-      <section>
+      <section className='bg-[#f2f1f1]'>
         <img src={HeroImage} alt='Hero Image' className='max-w-full h-auto' />
         <div className='flex items-center flex-col justify-center'>
           <h2 className='text-red-500 text-lg flex items-center justify-center mt-6'>
