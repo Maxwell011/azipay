6f9a1bdf8f4b53ca4b0313fc47aeea1803d8a107
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-10-06 22:09:46 +0100

    Added Pension checkbox

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -155,6 +155,26 @@ const Business = () => {
             </label>
           </div>
 
+          <div className='toggle-switch'>
+            <h2 className="paye">NSITF</h2>
+            <input type='checkbox' id='toggle' className='toggle-checkbox' />
+            <label htmlFor='toggle' className='toggle-label'>
+              <span className='switch-text'>No</span>
+              <span className='toggle-slider'></span>
+              <span className='switch-text'>Yes</span>
+            </label>
+          </div>
+          
+          <div className='toggle-switch'>
+            <h2 className="paye">NSITF</h2>
+            <input type='checkbox' id='toggle' className='toggle-checkbox' />
+            <label htmlFor='toggle' className='toggle-label'>
+              <span className='switch-text'>No</span>
+              <span className='toggle-slider'></span>
+              <span className='switch-text'>Yes</span>
+            </label>
+          </div>
+
           <br />
           <br />
           <br />
