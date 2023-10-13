cca328aceb8129d02533df3bd28fc543b37b8e3c
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-10-13 23:44:40 +0100

    Updated Terms Of Use

--- src/components/TermsOfUse.jsx
+++ src/components/TermsOfUse.jsx
@@ -3,10 +3,6 @@ const TermsOfUse = () => {
     <>
       <div>
         <h1>Terms and conditions to use our services And App</h1>
-        <p>
-          These are the terms and conditions that helps work together very well
-          at Eazi Pay App
-        </p>
       </div>
     </>
   );
