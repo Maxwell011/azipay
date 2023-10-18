864756b965af6549e165e9ec2356449e0963ddbf
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-10-18 21:01:36 +0100

    Tried to add new component

---
+++ src/components/New.jsx
@@ -0,0 +1,78 @@
+// import { GoBackBtn } from "../../components";
+import "../css/New.css";
+
+const New = () => {
+  return (
+    <>
+      {/* <GoBackBtn /> */}
+      <section className='fixed-deposit-container'>
+        <h1 className='fixed-deposit-heading'>Fixed Deposit Request</h1>
+
+        <div className='select-account'>
+          <label htmlFor='choose-account' className='choose-account'>
+            Select account to be debited
+          </label>
+
+          <div className='select'>
+            <select name='pets' className='bank-select'>
+              <option value=''>Select</option>
+              <option value=''>Globus</option>
+              <option value=''>Zenith</option>
+            </select>
+          </div>
+        </div>
+
+        <p className='place-info'>Placement Information</p>
+        <label htmlFor='duration'>Proposed Duration</label>
+        <br />
+        <select name='duration' className='duration'>
+          <option value=''>Select</option>
+          <option value='6 Months'>6 Months</option>
+          <option value='1 Year'>1 Year</option>
+          <option value='2 Year'>2 Year</option>
+          <option value='3 Year'>3 Year</option>
+          <option value='4 Year'>4 Year</option>
+          <option value='5 Year'>5 Year</option>
+        </select>
+        <br />
+        <label htmlFor='amount'>Amount</label>
+        <br />
+        <input className='amount2' placeholder='E.g $2,500,000.00' />
+        <br />
+
+        <label htmlFor='rate'>Rate (Based on duration & amount)</label>
+        <br />
+        <input id='rate' disabled placeholder='20%' className="rate-input" />
+        <br />
+
+        <p>Upon Maturity Tick as Appropriate</p>
+
+        <form>
+          <div className="return-choice">
+            <div className="first-choice">
+          <input type='radio' id='html' name='' value='HTML' />
+          <label htmlFor='interest'>Roll Over Interest and Principal</label>
+            </div>
+          <br />
+          <input type='radio' id='css' name='' value='CSS' />
+          <label htmlFor='interest'>
+            Drop Interest and Roll over Principal
+          </label>
+          <br />
+          <input
+            type='radio'
+            id='javascript'
+            name=''
+            value='JavaScript'
+          />
+          <label htmlFor='interest'>Drop Both Interest and Principal</label>
+          </div>
+        </form>
+
+        <button className="continue-btn">Continue</button>
+      </section>
+    </>
+  );
+};
+
+export default New;
