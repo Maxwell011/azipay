0bb1b1a84b23e328f8737b5bfdd917a679afded2
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-10-19 23:17:44 +0100

    Add new

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -92,70 +92,48 @@ import "../css/New.css";
 
 // export default New;
 
-// const New = () => {
-//   return (
-//     <>
-//       <div className='second-form'>
-//         <h1 className='second-form-heading'>Fixed Deposit Request</h1>
-
-//         <p className='confirm-request-heading'>Confirm your request</p>
-//         <div className='confirming-request'>
-//           <div className='confirm-details'>
-//             <h3 className='request-heading1'>Debited Account</h3>
-//             <p className='request-choice'>Savings - 0046329533</p>
-//           </div>
-//           <div className='confirm-details'>
-//             <h3 className='request-heading1'>Proposed Duration</h3>
-//             <p className='request-choice'>2 Years </p>
-//           </div>
-//           <div className='confirm-details'>
-//             <h3 className='request-heading1'>Amount</h3>
-//             <p className='request-choice'>2,500,000.00</p>
-//           </div>
-//           <div className='confirm-details'>
-//             <h3 className='request-heading2'>Rate</h3>
-//             <p className='request-choice'>12% per annum </p>
-//           </div>
-//           <div className='confirm-details'>
-//             <h3 className='request-heading2'>Maturity Value</h3>
-//             <p className='request-choice'>3,000,000.00</p>
-//           </div>
-//           <div className='confirm-details'>
-//             <h3 className='request-heading2'>Upon Maturity</h3>
-//             <p className='request-choice'>Roll Over Interest and Principal </p>
-//           </div>
-//           <div className='note-div'>
-//             <p className='note'>NOTE:</p>
-//             <p className='note'>
-//               Pre-liquidation will attract a 40% penalty on accrued interest.
-//             </p>
-//           </div>
-
-//           <div className='confirm-buttons'>
-//             <button className='previous-btn'>Previous</button>
-//             <button className='submit-btn'>Submit Request</button>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default New;
-
 const New = () => {
   return (
     <>
-      <div className='transaction-history-div'>
-        <div className='transaction-history-content'>
-          <h2>Fixed Deposit Investment</h2>
-          <div className='deposit-amount-container'>
-            <p className='deposit-amount'>Deposit Amount</p>
-            <p>NGN 2,500,000.00</p>
+      <div className='second-form'>
+        <h1 className='second-form-heading'>Fixed Deposit Request</h1>
+
+        <p className='confirm-request-heading'>Confirm your request</p>
+        <div className='confirming-request'>
+          <div className='confirm-details'>
+            <h3 className='request-heading1'>Debited Account</h3>
+            <p className='request-choice'>Savings - 0046329533</p>
           </div>
-          <div className='Maturity-value-container'>
-            <p className='Maturity-value'>Maturity Value</p>
-            <p>NGN 3,000,000.00</p>
+          <div className='confirm-details'>
+            <h3 className='request-heading1'>Proposed Duration</h3>
+            <p className='request-choice'>2 Years </p>
+          </div>
+          <div className='confirm-details'>
+            <h3 className='request-heading1'>Amount</h3>
+            <p className='request-choice'>2,500,000.00</p>
+          </div>
+          <div className='confirm-details'>
+            <h3 className='request-heading2'>Rate</h3>
+            <p className='request-choice'>12% per annum </p>
+          </div>
+          <div className='confirm-details'>
+            <h3 className='request-heading2'>Maturity Value</h3>
+            <p className='request-choice'>3,000,000.00</p>
+          </div>
+          <div className='confirm-details'>
+            <h3 className='request-heading2'>Upon Maturity</h3>
+            <p className='request-choice'>Roll Over Interest and Principal </p>
+          </div>
+          <div className='note-div'>
+            <p className='note'>NOTE:</p>
+            <p className='note'>
+              Pre-liquidation will attract a 40% penalty on accrued interest.
+            </p>
+          </div>
+
+          <div className='confirm-buttons'>
+            <button className='previous-btn'>Previous</button>
+            <button className='submit-btn'>Submit Request</button>
           </div>
         </div>
       </div>
@@ -164,3 +142,45 @@ const New = () => {
 };
 
 export default New;
+
+// const New = () => {
+//   return (
+//     <>
+//       <div className='transaction-history-div'>
+//         <div className='transaction-history-content'>
+//           <h2>Fixed Deposit Investment</h2>
+//           <div className='deposit-amount-container'>
+//             <p className='deposit-amount-heading'>Deposit Amount</p>
+//             <p className='amount-value'>NGN 2,500,000.00</p>
+//           </div>
+//           <div className='maturity-value-container'>
+//             <p className='maturity-value-heading'>Maturity Value</p>
+//             <p className='maturity-value'>NGN 3,000,000.00</p>
+//           </div>
+//           <div className='proposed-container'>
+//             <p className='proposed-heading'>Proposed Duration</p>
+//             <p className='proposed-value'>18 Months</p>
+//           </div>
+//           <div className='renewal-container'>
+//             <p className='renewal-heading'>Renewal Option</p>
+//             <p className='renewal-value'>Roll Over Interest and Capital</p>
+//           </div>
+//           <div className='interest-container'>
+//             <p className='interest-heading'>Interest Rate</p>
+//             <p className='interest-value'>20% Annually</p>
+//           </div>
+//           <div className='accrued-container'>
+//             <p className='accrued-heading'> Accrued Interest</p>
+//             <p className='accrued-value'>NGN 8,253,000</p>
+//           </div>
+//           <div className='payment-container'>
+//             <p className='payment-heading'>Interest Payment Frequency</p>
+//             <p className='payment-value'>Quarterly</p>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default New;
