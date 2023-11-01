20a24fd22ae9304c94b01b3d514c19832eda6388
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-11-01 22:09:42 +0100

    Added class to register page

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -1,10 +1,10 @@
 const PrivacyPolicy = () => {
   return (
     <div>
-      <h1>Welcome to the privacy page</h1>
+      <h1 className=''>Welcome to the privacy page</h1>
       <h1>Welcome to the privacy page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
